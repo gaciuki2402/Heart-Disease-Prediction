ccfe8971470b1eb8791d8a16af67b277585032a4
Grace Gaciuki <78306838+gaciuki2402@users.noreply.github.com>
2024-03-07 15:06:53 +0300

    Update heart-disease-predictor-web-app.py

--- heart-disease-predictor-web-app.py
+++ heart-disease-predictor-web-app.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-model_path = "/Heart_Disease_Model.pkl"
-data_path = "/heart_disease.csv"
+model_path = "Heart_Disease_Model.pkl"
+data_path = "heart_disease.csv"
 
 model = pk.load(open(model_path, "rb"))
 data = pd.read_csv(data_path)
